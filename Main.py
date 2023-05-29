c7cb743406149c2d1a0c482c76cc16b5098cb45d
rosaainz <al2173040136@azc.uam.mx>
2023-05-29 16:12:40 -0600

    Read file csv and process the order message

--- Main.py
+++ Main.py
@@ -4,34 +4,19 @@ import myers
 def calculate_total(message, catalog_file):
 	prices = {}
 
-	file = open(catalog_file).readlines()
-
-	#separar precio y producto en un diccionario
-	for line in file:
+	with open(catalog_file) as file:
+		lines = file.readlines()
+	
+	#procesar mensaje 
+	for line in lines[1:]:
 		line = line.strip()
-		print(line)
-		price, product = line.split(" ", 1)
-		price = price.strip("$")
+		price, product = line.split(",",1)
+		print(price)
+		print(product)
 		prices[product] = int(price)
 
-	#calcular total
-	#procesar el mensaje del pedido
-	lines_message = message.strip().split("\n")
-	print(lines_message)
-
-
-	total = 0
-	for line in lines_message:
-		print(line)
-		quantity, product = line.split(" ",1)
-		product = product.strip()
-
-		if product in prices:
-			catalog_price = prices[product]
-			total_product = int(quantity) * catalog_price
-			total += total_product
-	
-	return total
+	print(prices)
+	return 0
 
 message = """
 2 Berlinesas de Queso Mascarpone
